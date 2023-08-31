dbc208a43d67026dcbe0e96aae323979980e73f8
yolmane <yolmane@yandex.ru>
2023-08-31 22:25:36 +0300

    theme3_lesson7

---
+++ theme_3_lesson_7/1.py
@@ -0,0 +1,7 @@
+a1 = str.lower(input())
+a2 = a1[::-1]
+print(a2)
+if a1 == a2:
+    print("yes"),
+else:
+    print("no")
---
+++ theme_3_lesson_7/2.py
@@ -0,0 +1,3 @@
+a1 = str(input())
+a2 = " ".join(str.split(a1))
+print(a2)
